35cf9b4317d304eab93dc64afa1fabb8e44cf215
Piboule <Piboule>
2021-08-04 08:18:51 +0000

    git-svn-id: http://rdinnovation.onf.fr/svn/computreev6@209 b135c390-378b-4ba5-bdcd-e4540112eb5a

---
+++ create_plugin_pro.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo # Check dependencies for Computree plugins (paths, 3rd party libraries, compiler, options)> plugins.pro
+echo include(config/check_dependencies_plugins.pri)>> plugins.pro
+echo.>> plugins.pro
+echo TEMPLATE = subdirs>> plugins.pro
+echo SUBDIRS = plugins/pluginbase>> plugins.pro
+echo.>> plugins.pro
+echo # Comment/uncomment the plugins you want to use/compile. You can also add you own plugin here>> plugins.pro
+echo #SUBDIRS += ../plugingenerate/plugingenerate>> plugins.pro
+echo SUBDIRS += ../pluginmk/pluginmk>> plugins.pro
+echo SUBDIRS += ../pluginonf/pluginonf>> plugins.pro
+echo SUBDIRS += ../pluginsegma/pluginsegma>> plugins.pro
+echo SUBDIRS += ../pluginignlif/pluginignlif>> plugins.pro
+echo SUBDIRS += ../plugintoolkit/plugintoolkit>> plugins.pro
+echo.>> plugins.pro
