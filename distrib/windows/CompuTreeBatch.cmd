eb25d0e8821c4842a6eda6d8f06659f157868648
Piboule <Piboule>
2023-05-17 08:39:39 +0000

    ajout cmd batch
    git-svn-id: http://rdinnovation.onf.fr/svn/computreev6@381 b135c390-378b-4ba5-bdcd-e4540112eb5a

---
+++ distrib/windows/CompuTreeBatch.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+SET local=.\libraries\
+SET np=^
+%local%core;^
+%local%Qt;^
+%local%bin;^
+%local%gdal;^
+%local%opencv;^
+%local%pcl;
+
+SET PATH=%np%%path%
+
+cd "%~dp0"
+start CompuTreeBatch.exe %
+exit
