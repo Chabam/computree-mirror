46ca44d944bd2c29cfd372031d8b707fdbf8d8ea
Chabam <fchabot1337@gmail.com>
2025-02-17 15:24:27 +0100

    Made library appart (again) of the repo

--- external/dependencies.cmake
+++ external/dependencies.cmake
@@ -15,13 +15,25 @@ find_package(Qt5 COMPONENTS REQUIRED
 )
 
 find_package(Eigen3 REQUIRED)
+find_package(GDAL REQUIRED)
 find_package(muparser REQUIRED)
-find_package(OpenGL REQUIRED)
+find_package(OpenCV REQUIRED)
+find_package(PCL REQUIRED)
+find_package(Boost REQUIRED)
+find_package(Qhull REQUIRED)
+find_package(flann REQUIRED)
+find_package(GSL REQUIRED)
+find_package(LASzip REQUIRED)
 
 include(FetchContent)
 
-FetchContent_Declare(ctlibs
-    GIT_REPOSITORY https://github.com/Chabam/computreelibs
-    GIT_TAG        main
+# NOTE: The original authors of Computree did some changes to this fork...
+set(NodeEditorPatch git apply ${CMAKE_CURRENT_LIST_DIR}/computree-custom.patch)
+FetchContent_Declare(NodeEditor
+    GIT_REPOSITORY https://github.com/Daguerreo/NodeEditor
+    GIT_TAG 9bf1549e9bf1f4bf75c2a381402593c4a8b21a27
+    PATCH_COMMAND ${NodeEditorPatch}
+    UPDATE_DISCONNECTED 1
 )
-FetchContent_MakeAvailable(ctlibs)
+FetchContent_MakeAvailable(NodeEditor)
+find_package(nodes)
